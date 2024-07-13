4e07d7aead4cd3acd4546bff6385de602f11880a
Luis Alejandro Huayamave Cedeño <hbweyb@gmail.com>
2024-07-13 16:40:20 -0500

    Prueba commit

--- src/app/reports/pages/user-reports/user-reports.component.ts
+++ src/app/reports/pages/user-reports/user-reports.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-user-reports',
   templateUrl: './user-reports.component.html',
-  styles: ``
+  styles: ``,
 })
 export class UserReportsComponent {
-
+  generatePDF() {
+    console.log('Button was clicked');
+  }
 }
